7e088ad1daf365385986f87fcc1a64bbabac9766
username <ya.pasha.verbenko@yandex.ru>
2025-04-10 11:52:50 +0300

    Добавил в класс Bot дороботку, теперь бот может отвечать тебе! Если ты ему напишешь привет или что-то другое, он напишет тебе привет и твоё имя!

--- src/main/java/src/pavelverbenko/Interview__Bot/telegram/Bot.java
+++ src/main/java/src/pavelverbenko/Interview__Bot/telegram/Bot.java
@@ -1,16 +1,30 @@
 package src.pavelverbenko.Interview__Bot.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 //тут мы обработываем обновленя, которые нам прилетают с telegram
 @Component
 public class Bot extends TelegramLongPollingBot {
 
+    public Bot(@Value("${bot.token}") String token) {
+        super(token);
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
-
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("Привет, " + update.getMessage().getFrom().getFirstName() + "!");
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
